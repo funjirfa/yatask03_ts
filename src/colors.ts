f1520d80dcc779160406fc5ef105664620f085de
Anton <racer-loser@mail.ru>
2021-10-14 22:26:01 +0300

    first ts script

---
+++ src/colors.ts
@@ -0,0 +1,35 @@
+import { backgroundColors, effects, fontColors, Reset } from './model';
+function addColor(text: string, color: string, isBackground = false) {
+    if (isBackground) {
+        return text + backgroundColors[color];
+    }
+    return text + fontColors[color];
+}
+function getEffects(effectList: string[]) {
+    return effectList.map((effect: string) => effects[effect]).join('');
+}
+
+export type ColorOptions = {
+    font?: string;
+    background?: string;
+    effects?: string[];
+}
+export function color(text: string, options: ColorOptions) {
+    const preparedText = text.replace(/ё/g, 'е');
+    let result = '';
+    if (options) {
+        if (options.font) {
+            result = addColor(result, options.font);
+        }
+        if (options.background) {
+            result = addColor(result, options.background, true);
+        }
+        if (options.effects) {
+            result += getEffects(options.effects);
+        }
+        result += preparedText;
+        result += Reset;
+        return result;
+    }
+    return preparedText;
+}
